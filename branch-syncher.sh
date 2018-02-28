3262ce147e2501b07e7e0dfed4f1d484cbdc9b66
uğur mirza zeyrek <github@mersinuni.net>
2018-02-28 22:09:09 +0200

    removed --no-ff for preventing loops.

--- branch-syncher.sh
+++ branch-syncher.sh
@@ -28,13 +28,13 @@ git pull origin $CURRENT_BRANCH
 echo "Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to sub-branches"
 case $CURRENT_BRANCH in
 production)
-  ( git checkout -f master && git pull origin master && git merge --no-ff $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin master ) || ( echo "auto merge failed." && exit 1 )
+  ( git checkout -f master && git pull origin master && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin master ) || ( echo "auto merge failed." && exit 1 )
   ;;
 master)
-  ( git checkout -f staging && git pull origin staging && git merge --no-ff $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin staging ) || ( echo "auto merge failed." && exit 1 )
+  ( git checkout -f staging && git pull origin staging && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin staging ) || ( echo "auto merge failed." && exit 1 )
   ;;
 staging)
-  ( git checkout -f development && git pull origin development && git merge --no-ff $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin development ) || ( echo "auto merge failed." && exit 1 )
+  ( git checkout -f development && git pull origin development && git merge $CURRENT_BRANCH -m "auto merge with $CURRENT_BRANCH" && git push origin development ) || ( echo "auto merge failed." && exit 1 )
   ;;
 esac
 
