9e612fbd0292bce886fa5352fa2fe33bb8aa2e8c
uğur mirza zeyrek <github@mersinuni.net>
2018-03-01 13:20:59 +0300

    added repository fetch lines.

--- php-syntax-checker.sh
+++ php-syntax-checker.sh
@@ -9,6 +9,14 @@ pwd
 
 GIT_PREVIOUS_COMMIT=$1
 GIT_COMMIT=$2
+REPOSITORY_URL=$3
+# stripping https://github.com/
+REPOSITORY_NAME=${REPOSITORY_URL:19}
+
+git remote set-url origin git@github.com:${REPOSITORY_NAME}
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+echo "Fetching all"
+git fetch --all
 
 echo "changed files"
 echo git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT
