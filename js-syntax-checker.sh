ad5e57344b15262869c3925e287fa2924b9942b0
Hendrik <ferox@hotmail.com>
2021-11-15 17:39:40 +0100

    Fix js check

--- js-syntax-checker.sh
+++ js-syntax-checker.sh
@@ -10,29 +10,40 @@ pwd
 GIT_PREVIOUS_COMMIT=$1
 GIT_COMMIT=$2
 REPOSITORY_URL=$3
-if [ $GIT_PREVIOUS_COMMIT = $GIT_COMMIT ] || [ $GIT_PREVIOUS_COMMIT = "" ]
-then
+if [ $GIT_PREVIOUS_COMMIT = $GIT_COMMIT ] || [ $GIT_PREVIOUS_COMMIT = "" ]; then
   # let's assume going back to 30 commits would be enough for covering even an exceptional huge PR case.
   GIT_PREVIOUS_COMMIT=$(git rev-list -30 --skip=29 --max-count=1 HEAD)
 fi
+
+echo "Set ownership to deploy"
+sudo chown deploy:www-data . -R
+
 # stripping https://github.com/
 REPOSITORY_NAME=${REPOSITORY_URL:19}
-sudo git config --unset-all remote.origin.fetch
-sudo git remote set-url origin git@github.com:${REPOSITORY_NAME}
-sudo git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+git config --unset-all remote.origin.fetch
+git remote set-url origin git@github.com:${REPOSITORY_NAME}
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 echo "track all"
-sudo git branch -r | grep -v '\->' | while read remote; do sudo git branch --track "${remote#origin/}" "$remote"; done
+git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 
 echo "Fetching all"
-sudo git fetch --all
+git fetch --all
 
 echo "All changed files"
-sudo git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT
+git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT
 
-# show different php files only
-echo "Changed js files"
-sudo git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep .js$
+# show different js files only
+changedjs=$(git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep .js$ | grep -v wpml-translation-management/ | grep -v node_modules/ | grep -v vendor/ | grep -v gulpfile.babel.js)
 
-sudo git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep .js$ | grep -v wpml-translation-management/ | grep -v node_modules/ | grep -v vendor/ | grep -v gulpfile.babel.js | xargs -n1 echo esvalidate | bash | grep -v "No syntax errors detected" && echo "JavaScript Syntax error(s) detected" && exit 1
+# only run esvalidate where there are changes
+if [[ -n $changedjs ]]; then
+  echo "Checking syntax of modified js files.."
+  echo "Using eslint -v"
+  eslint -v || sudo npm install -g eslint
+  
+  git diff --diff-filter=ACMR --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep .js$ | grep -v wpml-translation-management/ | grep -v node_modules/ | grep -v vendor/ | grep -v gulpfile.babel.js | xargs -n1 echo eslint --no-eslintrc --env es6 --parser-options=ecmaVersion:8 | bash 
+else
+  echo "No JS modifications found, skipping syntax checks."
+fi
 
 echo "no js syntax errors detected" && exit 0
